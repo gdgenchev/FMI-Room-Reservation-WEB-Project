0186f4e105e09afad1be2f1b076b6fc012dcce12
gdgenchev <gdgenchev97@gmail.com>
2020-02-12 17:41:01 +0200

    Use prepared statement to prevent SQL Injection

--- project/php/persistence/reservation/ReservationRepositorySQL.php
+++ project/php/persistence/reservation/ReservationRepositorySQL.php
@@ -13,19 +13,17 @@ class ReservationRepositorySQL implements ReservationRepository
     function getAvailableRooms($startDateTime, $endDateTime)
     {
         $sql = "SELECT room.type, room.roomNumber, room.buildingName from room left join (select * from reservation where
-              '$startDateTime' > reservedFrom AND 
-              '$startDateTime' < reservedTo OR
-              '$endDateTime' > reservedFrom AND
-              '$endDateTime' < reservedTo) reservedRoom
+              :startDateTime > reservedFrom AND 
+              :startDateTime < reservedTo OR
+              :endDateTime > reservedFrom AND
+              :endDateTime < reservedTo) reservedRoom
                ON room.roomNumber = reservedRoom.roomNumber
                AND room.buildingName = reservedRoom.buildingName
                WHERE reservedFrom is NULL";
-        $test = 'SELECT * from room';
-       return $this->conn->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
+       return $this->conn->prepare($sql)
+           ->execute(['startDateTime' => $startDateTime, 'endDateTime' => $endDateTime])
+           ->fetchAll(PDO::FETCH_ASSOC);
     }
-
-
-
 }
 
