bc5f146a68c6738b96e6292cafe685de3c9af4af
gdgenchev <gdgenchev97@gmail.com>
2020-05-10 15:28:44 +0300

    Refactor brackets

--- project/js/calendar/calendar.js
+++ project/js/calendar/calendar.js
@@ -1,73 +1,61 @@
-$('.custom-file-input').on('change', function() {
-	let fileName = $(this).val().split('\\').pop();
-	$(this).next('.custom-file-label').addClass("selected").html(fileName);
+$('.custom-file-input').on('change', function () {
+    let fileName = $(this).val().split('\\').pop();
+    $(this).next('.custom-file-label').addClass("selected").html(fileName);
 });
 
-$( document ).ready(function()
-	{
+$(document).ready(function () {
 
-	  $("#search_button").click(function()
-	  {
-	  	$("#myform").submit(function()
-		  {
-				var info = $("#info").val().split(" ");
-				var obj = {};
-				for (var i = 0; i < info.length; i++) 
-				{
-			  		obj[i.toString()] = info[i];
-			  	}
-			    $("#mytable").load("../php/getInfo.php", obj);
-			     return false;
-		  });
-	  });
+    $("#search_button").click(function () {
+        $("#myform").submit(function () {
+            var info = $("#info").val().split(" ");
+            var obj = {};
+            for (var i = 0; i < info.length; i++) {
+                obj[i.toString()] = info[i];
+            }
+            $("#mytable").load("../php/getInfo.php", obj);
+            return false;
+        });
+    });
 
-	  $("#import_button").click(function()
-	  {
-	  	$("#myform").submit(function()
-		  {	
-		   var file = $("#imported_file")[0].files[0];
-		  	var extension = $("#imported_file").val().split('.').pop();
-		  	console.log(extension);
-		  	if( document.getElementById("imported_file").files[0].size >0 && extension.localeCompare("csv") === 0)
-		  	{
-		  		var formData = new FormData();
-		  		formData.append('file',file);
-		  		$.ajax({
-			  		type: "POST",
-			  		url: "../php/import.php",
-			  		contentType: false,
-			  		processData: false,
-			  		cache: false,
-			  		data: formData,
+    $("#import_button").click(function () {
+        $("#myform").submit(function () {
+            var file = $("#imported_file")[0].files[0];
+            var extension = $("#imported_file").val().split('.').pop();
+            console.log(extension);
+            if (document.getElementById("imported_file").files[0].size > 0 && extension.localeCompare("csv") === 0) {
+                var formData = new FormData();
+                formData.append('file', file);
+                $.ajax({
+                    type: "POST",
+                    url: "../php/import.php",
+                    contentType: false,
+                    processData: false,
+                    cache: false,
+                    data: formData,
 
-			  		success:function(response)
-			  		{
-			  			if(response === "success")
-			  			{
-			  				alert("File is successfully uploaded!");
-			  			}
-			  			else 
-			  			{
-			  				alert(response);
-			  			}
-			  		}
-		  		});	  	 
-		  	}
-		  	return false;
-		  });
-	  });
+                    success: function (response) {
+                        if (response === "success") {
+                            alert("File is successfully uploaded!");
+                        } else {
+                            alert(response);
+                        }
+                    }
+                });
+            }
+            return false;
+        });
+    });
 
-	  $("#export_button").click(function()
-	  {
-	  	$("#myform").submit(function(){
+    $("#export_button").click(function () {
+        $("#myform").submit(function () {
 
-	  		$.ajax({
-	  			type: "POST",
-			  	url: "../php/export.php"
-	  		});
+            $.ajax({
+                type: "POST",
+                url: "../php/export.php"
+            });
 
-	  		return false;
-	  	});
-	  });
+            return false;
+        });
+    });
 
-	});
+});
